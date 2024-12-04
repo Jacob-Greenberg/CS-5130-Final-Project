ca4a6b7b7dfdf1826a0f051e90295d0bee955391
Jacob Greenberg <101679277+Jacob-Greenberg@users.noreply.github.com>
2024-12-04 16:19:18 -0500

    Getting interfacing working

---
+++ adb_interface.py
@@ -0,0 +1,40 @@
+import subprocess
+
+class TouchInterface:
+    """ This class handles screen inputs. It assumes that only one devices is connected via ADB """
+    def __init__(self):
+        self.adb_path = "C:\\Users\\green\\Desktop\\Advanced Software Engineering - CS 5130\\Final Project\\platform-tools\\adb.exe"
+
+    def touch(self, x: int, y: int):
+        """
+        A single tap anywhere on the screen
+        """
+        subprocess.run([self.adb_path, "shell", "input", "tap", str(x), str(y)])
+
+    def swipe(self, x_start: int, y_start: int, x_end: int, y_end: int, time: int):
+        """
+        Swipe from the starting coordinates to the ending ones, over a given time (in ms)
+        """
+        subprocess.run([self.adb_path, "shell", "input", "swipe", str(x_start), str(y_start), str(x_end), str(y_end), str(time)])
+
+    def text(self, text: str):
+        """
+        Enter a string of text into a text field (must already be selected)
+        """
+        subprocess.run([self.adb_path, "shell", "input", "text", text])
+
+    def key(self, keycode: int):
+        """
+        Enter a keypress
+        """
+        # 3 -> Home Button
+        # 4 -> Back Button
+        subprocess.run([self.adb_path, "shell", "input", "keyevent", keycode])
+
+    def dump_ui_hierarchy(self, path: str = "./"):
+        """
+        Get an XML readout of the current elements on the screen and download it to the given path
+        """
+        subprocess.run([self.adb_path, "shell", "uiautomator", "dump"])
+        subprocess.run([self.adb_path, "pull", "//sdcard//window_dump.xml", path])
+        
--- automator.py
+++ automator.py
@@ -1,14 +1,11 @@
 import os
 import json
-from uiautomator import Device
 from ollama import chat, ChatResponse, Client
+from adb_interface import TouchInterface
 
 PROMPT_FILE_NAME = "prompt.txt"
-DEVICE_SERIAL = '5110016020032640'
-LLM_MODEL = 'llama2-uncensored'
+LLM_MODEL = 'llama3.2:3b' #'llama2-uncensored'
 OLLAMA_HOST_IP = '127.0.0.1:11434'
-
-
 
 # Load the system prompt from the local file
 system_prompt = ""
@@ -21,54 +18,65 @@ user_prompt = input("Enter the condition you would like to be tested: ")
 prompt = system_prompt + user_prompt + "\n###END TESTER PROMPT###"
 
 llm_client = Client(
-    host=OLLAMA_HOST_IP
+    host=OLLAMA_HOST_IP,
+    timeout = 300
 )
+
+ti = TouchInterface()
 
 def enter_input():
 
     # Get current screen XML
-    phone.dump("hierarchy.xml")
+    ti.dump_ui_hierarchy()
 
     # Make it useful to us
     hierarchy = ""
-    with open('hierarchy.xml') as file:
+    with open('window_dump.xml') as file:
         hierarchy = hierarchy.join(file)
     
     # Plug everything into a chat
-    response: ChatResponse = llm_client.chat(model=LLM_MODEL, messages=[{'role':'user','content':prompt + hierarchy}])
+    response: ChatResponse = llm_client.chat(model=LLM_MODEL, messages=[{'role':'user','content': hierarchy}, {'role':'system', 'content':prompt}])
+    print("==========")
+    print(response)
+    print("==========")
     response_json = json.loads(response['message']['content'])
-    print(response_json)
     return response_json
 
-phone = Device(DEVICE_SERIAL) # Get a reference to the device
+def preform_action(action: json):
+    try:
+        if 'touch' in action['command']: # touch <X Pixel> <Y Pixel>
+            x = action['command'].split(" ")[1]
+            y = action['command'].split(" ")[2]
+            ti.touch(x, y)
 
+        elif 'swipe' in action['command']: # swipe <X Starting Pixel> <Y Starting Pixel> <X Ending Pixel> <Y Endind Pixel> <Duration (ms)>
+            x_start  = action['command'].split(" ")[1]
+            y_start  = action['command'].split(" ")[2]
+            x_end    = action['command'].split(" ")[3]
+            y_end    = action['command'].split(" ")[4]
+            duration = action['command'].split(" ")[5]
+            ti.swipe(x_start, y_start, x_end, y_end, duration)
+
+        elif 'text' in action['command']: # text <text to enter>
+            text = action['command'].split(" ")[1]
+            ti.text(text)
+
+        elif 'key' in action['command']: # key <android/adb keycode>
+            keycode = action['command'].split(" ")[1]
+            ti.key(keycode)
+
+        else:
+            # AI gave us an invalid response :(
+            pass
+    except IndexError as e:
+        pass
+        # AI gave us an invalid response
+
+print("Querying LLM")
 response_json = enter_input()
+preform_action(response_json)
 
 while(response_json['command'] != "end" or response_json['command'] != "error"):
     
-    response_json = enter_input
-    print(response_json)
-    phone.press(response_json['command'])
-
-
-
-
-
-#phone.press()
-
-#COMMAND_MAP = {
-#"home": phone.press.home(),
-#"back": phone.press.back(),
-#"left": phone.press.left(),
-#"right": phone.press.right(),
-#"up": phone.press.up(),
-#"down": phone.press.down(),
-#"center": phone..press,
-#"menu": phone.,
-#"search": phone.,
-#"enter": phone.,
-#"delete": phone.,
-#"recent": phone.,
-#"camera": phone.,
-#"power": phone.,
-#}
+    response_json = enter_input()
+    preform_action(response_json)
